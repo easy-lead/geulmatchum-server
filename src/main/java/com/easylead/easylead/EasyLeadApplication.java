330726143341106cda1a1480d539d16c9a1159ca
oU-Ua <imuu30071126@gmail.com>
2024-07-06 21:36:28 +0900

    Initial Commit

---
+++ src/main/java/com/easylead/easylead/EasyLeadApplication.java
@@ -0,0 +1,13 @@
+package com.easylead.easylead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyLeadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easylead/easylead/EasyLeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easylead.easylead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLeadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
